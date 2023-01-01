b667afb8d37ac40765d28273c6a84f9746f03b4d
ElizPN <pankevych.liza@gmail.com>
2023-01-01 16:42:44 +0100

    add input and button

--- src/AddCity.tsx
+++ src/AddCity.tsx
@@ -1,3 +1,23 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+
 export function AddCity() {
-  return <input />;
+  return (
+    <Grid
+      container
+      sx={{
+        width: 500,
+        maxWidth: "100%",
+        m: 10,
+      }}
+    >
+      <Grid item xs={10}>
+        <TextField fullWidth label='Search city or area ' id='fullWidth' />
+      </Grid>
+      <Grid item xs={2}>
+        <Button variant='outlined'>Search</Button>
+      </Grid>
+    </Grid>
+  );
 }
