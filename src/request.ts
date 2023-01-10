911cf065a3e457c0ec9c15a20f4f3f1db7f3b746
ElizPN <pankevych.liza@gmail.com>
2023-01-10 10:41:21 +0100

    fix parameter (change irl to cityValue)

--- src/request.ts
+++ src/request.ts
@@ -1,6 +1,8 @@
-export const fetchWeatherData = async (dataUrl: string) => {
+const apiKey = "936a43fe9c1da3254004f3c7a1c14348";
+
+export const fetchWeatherData = async (cityValue: string) => {
+  let dataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
   return fetch(dataUrl).then((response: Response) => {
     return response.json();
   });
-
 };
