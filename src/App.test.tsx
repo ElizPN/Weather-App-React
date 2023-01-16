c82fd85f140ace2d2471147a4c999af265113f9d
ElizPN <pankevych.liza@gmail.com>
2023-01-16 11:05:17 +0100

    switch tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,11 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("TextField for adding city exists", () => {
   render(<App />);
-  const addCityTextField = screen.getByTestId("add-city");
-  expect(addCityTextField).toBeInTheDocument();
+  // const addCityTextField = screen.getByTestId("add-city");
+  // expect(addCityTextField).toBeInTheDocument();
+
+  const titleText = screen.getByTestId("title");
+  expect(titleText).toBeInTheDocument();
 });
