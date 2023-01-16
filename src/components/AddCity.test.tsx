2bc54cac43ab9b6123773409a012bec409e62f59
ElizPN <pankevych.liza@gmail.com>
2023-01-16 14:09:15 +0100

    test does handler is called after button click

--- src/components/AddCity.test.tsx
+++ src/components/AddCity.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { AddCity } from "./AddCity";
 
 describe("AddCity", () => {
@@ -20,20 +20,38 @@ describe("AddCity", () => {
     expect(addCityTextField).toBeInTheDocument();
   });
 
-   test("Add button exists", () => {
-     const handeOnChange = jest.fn();
-     const handleOnclick = jest.fn();
+  test("Add button exists", () => {
+    const handeOnChange = jest.fn();
+    const handleOnclick = jest.fn();
 
-     render(
-       <AddCity
-         inputValue={""}
-         err={""}
-         sameCityMessage={""}
-         handeOnChange={handeOnChange}
-         handleOnclick={handleOnclick}
-       />
-     );
-     const addButton = screen.getByTestId("add-button");
-     expect(addButton).toBeInTheDocument();
-   });
+    render(
+      <AddCity
+        inputValue={""}
+        err={""}
+        sameCityMessage={""}
+        handeOnChange={handeOnChange}
+        handleOnclick={handleOnclick}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+    expect(addButton).toBeInTheDocument();
+  });
+
+  test("After click on button handleOnclick is called", () => {
+    const handeOnChange = jest.fn();
+    const handleOnclick = jest.fn();
+
+    render(
+      <AddCity
+        inputValue={"Barcelona"}
+        err={""}
+        sameCityMessage={""}
+        handeOnChange={handeOnChange}
+        handleOnclick={handleOnclick}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+    fireEvent.click(addButton);
+    expect(handleOnclick).toBeCalled();
+  });
 });
