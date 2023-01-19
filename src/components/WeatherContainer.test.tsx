0906c89e7abeda216322245a84f8dc7bf1720bf2
ElizPN <pankevych.liza@gmail.com>
2023-01-19 10:31:08 +0100

    test 404 error

--- src/components/WeatherContainer.test.tsx
+++ src/components/WeatherContainer.test.tsx
@@ -1,36 +1,28 @@
 import "@testing-library/jest-dom/extend-expect";
-import {
-  screen,
-  render,
-  fireEvent,
-  waitFor,
-  act,
-  getByTestId,
-} from "@testing-library/react";
+import { screen, render, fireEvent, act } from "@testing-library/react";
 import { WeatherContainer } from "./WeatherContainer";
 
 describe("WeatherContainer", () => {
-
-const mockResponse = {
-  cod: 200,
-  weather: [{ description: "clear sky" }],
-  main: { temp: 15 },
-  sys: { country: "ES" },
-  name: "Barcelona",
-};
-
-
+  const mockResponse = {
+    cod: 200,
+    weather: [{ description: "clear sky" }],
+    main: { temp: 15 },
+    sys: { country: "ES" },
+    name: "Barcelona",
+  };
 
   const fetchFakeWeatherdata = jest.fn().mockImplementation(() => {
-    Promise.resolve(mockResponse);
+    return Promise.resolve(mockResponse);
   });
 
-  test("get data from api", async () => {
-  
+  const mockError = { cod: "404", message: "city not found" };
 
+  const fetchFakeError = jest.fn().mockImplementation(() => {
+    return Promise.resolve(mockError);
+  });
 
+  test("after clicking the Add button, display a CityCard with the city entered in the input", async () => {
     render(<WeatherContainer fetchWeatherData={fetchFakeWeatherdata} />);
-    console.log(fetchFakeWeatherdata);
 
     const addCityTextField = screen.getByTestId("add-city-field");
     act(() => {
@@ -40,11 +32,32 @@ const mockResponse = {
     });
 
     expect(addCityTextField).toBeInTheDocument();
+    
     const addButton = screen.getByTestId("add-button");
     act(() => {
       fireEvent.click(addButton);
     });
 
     expect(await screen.findByTestId("city-card-0")).toBeInTheDocument();
+  });
+
+  test("error 404", async () => {
+    render(<WeatherContainer fetchWeatherData={fetchFakeError} />);
+
+    const addCityTextField = screen.getByTestId("add-city-field");
+    act(() => {
+      fireEvent.change(addCityTextField, {
+        target: { value: "incorrect city" },
+      });
+    });
+
+    expect(addCityTextField).toBeInTheDocument();
+
+    const addButton = screen.getByTestId("add-button");
+    act(() => {
+      fireEvent.click(addButton);
+    });
+
+    expect(await screen.findByTestId("404-error")).toBeInTheDocument();
   });
 });
