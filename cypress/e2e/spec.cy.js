97bc8a99ea3074a281a783ef8c1fbf0db0dde27a
ElizPN <pankevych.liza@gmail.com>
2023-01-20 17:02:01 +0100

    test rendering city card after adding city

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
 describe("My First Test", () => {
-  it("Visits the Kitchen Sink", () => {
+  it("should render city card after adding city", () => {
     cy.visit("https://elizpn.github.io/Weather-App-React/");
+
+    cy.contains("Add location");
+
+    cy.get("[data-testid=add-city-field]").type("Oslo");
+    cy.get("[data-testid=add-button]").click();
+
+    cy.get("[data-testid=city-card-0]").contains("Oslo");
   });
 });
