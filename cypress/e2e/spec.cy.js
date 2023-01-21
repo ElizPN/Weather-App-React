029151acd98eabc9881ad167b07e14cc79c4a9ba
ElizPN <pankevych.liza@gmail.com>
2023-01-21 17:36:38 +0100

    test rendering error message with invalid value

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe("My First Test", () => {
+describe("WeatherContainer", () => {
   it("should render city card after adding city", () => {
     cy.visit("https://elizpn.github.io/Weather-App-React/");
 
@@ -9,4 +9,18 @@ describe("My First Test", () => {
 
     cy.get("[data-testid=city-card-0]").contains("Oslo");
   });
+
+
+   it("should render error message with invalid value ", () => {
+     cy.visit("https://elizpn.github.io/Weather-App-React/");
+
+     cy.contains("Add location");
+
+     cy.get("[data-testid=add-city-field]").type("I wanna beer");
+     cy.get("[data-testid=add-button]").click();
+
+     cy.get("[data-testid=404-error]").contains(
+       "Please search for a valid city!"
+     );
+   });
 });
