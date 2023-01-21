6ba003897c4ad67ffe739718b1f25e8b66b31776
ElizPN <pankevych.liza@gmail.com>
2023-01-21 17:43:35 +0100

    test message same city when city  already exists

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,17 +10,34 @@ describe("WeatherContainer", () => {
     cy.get("[data-testid=city-card-0]").contains("Oslo");
   });
 
+  it("should render error message with invalid value ", () => {
+    cy.visit("https://elizpn.github.io/Weather-App-React/");
 
-   it("should render error message with invalid value ", () => {
-     cy.visit("https://elizpn.github.io/Weather-App-React/");
+    cy.contains("Add location");
 
-     cy.contains("Add location");
+    cy.get("[data-testid=add-city-field]").type("I wanna beer");
+    cy.get("[data-testid=add-button]").click();
 
-     cy.get("[data-testid=add-city-field]").type("I wanna beer");
-     cy.get("[data-testid=add-button]").click();
+    cy.get("[data-testid=404-error]").contains(
+      "Please search for a valid city!"
+    );
+  });
 
-     cy.get("[data-testid=404-error]").contains(
-       "Please search for a valid city!"
-     );
-   });
+  it("should render message about same city when entered city already exists", () => {
+    cy.visit("https://elizpn.github.io/Weather-App-React/");
+
+    cy.contains("Add location");
+
+    cy.get("[data-testid=add-city-field]").type("Oslo");
+    cy.get("[data-testid=add-button]").click();
+
+    cy.get("[data-testid=city-card-0]").contains("Oslo");
+
+    cy.get("[data-testid=add-city-field]").type("Oslo");
+    cy.get("[data-testid=add-button]").click();
+
+    cy.get("[data-testid='same-city-message']").contains(
+      "The weather of this city is already shown 😉"
+    );
+  });
 });
